af6c173710603d432b339b38682d6b91844d7c28
SALMAN <elfadili.salman@gmail.com>
2019-02-13 02:23:24 +0100

    Setup Spring boot application

---
+++ src/main/java/com/springboot/googlemaps/GooglemapsApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.googlemaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglemapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglemapsApplication.class, args);
+	}
+
+}
+
