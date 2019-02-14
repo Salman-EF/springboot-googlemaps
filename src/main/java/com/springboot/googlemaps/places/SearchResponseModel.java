74bf862f3835ece2d36de52abef3d9fc56f53d4e
SALMAN <elfadili.salman@gmail.com>
2019-02-14 22:23:06 +0100

    Built SearchResponseModel that present Response placesList and nextPage Existance

--- src/main/java/com/springboot/googlemaps/places/SearchResponseModel.java
+++ src/main/java/com/springboot/googlemaps/places/SearchResponseModel.java
@@ -3,5 +3,33 @@
  **/
 package com.springboot.googlemaps.places;
 
+import com.google.maps.model.PlacesSearchResult;
+
+import java.util.List;
+
 public class SearchResponseModel {
+
+    private List<PlacesSearchResult> places;
+    private boolean nextPageExists;
+
+    public SearchResponseModel(List<PlacesSearchResult> places, boolean nextPage) {
+        this.places = places;
+        this.nextPageExists = nextPage;
+    }
+
+    public List<PlacesSearchResult> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(List<PlacesSearchResult> places) {
+        this.places = places;
+    }
+
+    public boolean isNextPageExists() {
+        return nextPageExists;
+    }
+
+    public void setNextPage(boolean nextPageExists) {
+        this.nextPageExists = nextPageExists;
+    }
 }
